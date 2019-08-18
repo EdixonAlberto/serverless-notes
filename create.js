b7f2c471cacd077cfaef357dfc03484d124a4795
Edixon Alberto <edixonalbertto@gmail.com>
2019-08-18 03:29:19 -0400

    refactorizando todo todo el codigo
    - se abstrae el uso del obj dinamoDb en modulo dinamoDb-lib
    - se crea modulo response-lib para establecer las respuestas
      de los end-point
    - se implementan los modulos en la funcion create usado async/await
      gracias al uso de promesas en dinamoDb

--- create.js
+++ create.js
@@ -1,13 +1,11 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-AWS.config.update({ region: "us-east-2" });
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context) {
     // El request body se pasa como una cadena codificada en JSON en 'event.body'
     const data = JSON.parse(event.body);
-
     /*
         - Item: contiene los atributos del elemento a crear
         - userId: las identificaciones de usuario de 'cognito identity pool',
@@ -17,7 +15,6 @@ export function main(event, context, callback) {
         - attachment: adjunto del request body
         - createAt: marca de tiempo actual en formato UNIX
     */
-
     const params = {
         TableName: "notes",
         Item: {
@@ -29,30 +26,39 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // Establecer headers de respuesta para habilitar CORS (cross-origin resource sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        // Retornar codigo de estado 500 por error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-            callback(null, response);
-            return;
-        }
-
-        // Retornar codigo de estado 200 y el item recientemente creado
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
+    try {
+        await dynamoDbLib.call('put', params);
+        return success(params.Item);
+    } catch (error) {
+        console.log(error);
+        return failure({ status: false });
+    }
 }
+
+// NOT REFACTORY
+//     dynamoDb.put(params, (error, data) => {
+//         // Establecer headers de respuesta para habilitar CORS (cross-origin resource sharing)
+//         const headers = {
+//             "Access-Control-Allow-Origin": "*",
+//             "Access-Control-Allow-Credentials": true
+//         };
+
+//         // Retornar codigo de estado 500 por error
+//         if (error) {
+//             const response = {
+//                 statusCode: 500,
+//                 headers: headers,
+//                 body: JSON.stringify({ status: false })
+//             };
+//             callback(null, response);
+//             return;
+//         }
+
+//         // Retornar codigo de estado 200 y el item recientemente creado
+//         const response = {
+//             statusCode: 200,
+//             headers: headers,
+//             body: JSON.stringify(params.Item)
+//         };
+//         callback(null, response);
+//     });
